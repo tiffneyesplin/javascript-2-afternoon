abdd88d11641825cec58e0d44f3ec19ab847de6c
Tiffney Esplin <tiffneyesplin@Tiffneys-MacBook-Pro.local>
2019-01-08 14:53:02 -0700

    day two first test done

--- practice.js
+++ practice.js
@@ -14,8 +14,11 @@
   Then alert your name using dot notation.
 */
 
-//Code here
-
+ let me = {
+   name: "Tiffney",
+   age: 42,
+ }
+alert (me.name);
 
 
 ////////// PROBLEM 2 //////////
@@ -25,7 +28,14 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
+let favoriteThings = {
+  band: "Blake Shelton",
+  food: "Indian",
+  person: "Mat",
+  book: "The Secret Garden",
+  movie: "One Fine Day",
+  holiday: "July 4th",
+};
 
 
 
@@ -33,7 +43,8 @@
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = "Volvo" ;
+favoriteThings.brand = "Nike";
 
 
 
@@ -41,6 +52,8 @@
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
+favoriteThings.food = "Chicken Nuggets";
+favoriteThings.book = "Harry Potter";
 //Code here
 
 
@@ -55,15 +68,17 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
-
+backPack = {}
+var item = "firstPocket";
+backPack [item] = "chapstick";
+backPack.color = "Orange";
 
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+alert (backPack);
 
 
 
@@ -73,7 +88,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
+console.log (backPack);
 
 
 
@@ -96,7 +111,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = 'Tyler S. McGinnis';
+user2.email = 'tyler.mcginnis@devmounta.in';
 
 
 
